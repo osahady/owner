ab6f2fc393a176f0e38f247b23c481c704251740
Osa Hady <osahady@gmail.com>
2021-06-25 13:32:29 +0300

    user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,9 +15,9 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'=> $this->id,
+            'id' => $this->id,
             'name' => $this->name,
-            'email' => $this->email,
+            'phone' => $this->phone,
         ];
     }
 }
